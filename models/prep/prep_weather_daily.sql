aca8300493d587fba4ce70b60ffae38b377ea946
Meysam Goodarzi <analytica@meysam-goodarzi.com>
2024-12-11 12:05:55 +0100

    add first prep file

---
+++ models/prep/prep_weather_daily.sql
@@ -0,0 +1,27 @@
+WITH daily_data AS (
+    SELECT * 
+    FROM {{ref('staging_weather_daily')}}
+),
+add_features AS (
+    SELECT *
+		, DATE_PART('day', date) AS date_day
+		, DATE_PART('month', date) AS date_month
+		, DATE_PART('year', date) AS date_year
+		, DATE_PART('week', date) AS cw
+		, TO_CHAR(date, 'FMMonth') AS month_name
+		, TO_CHAR(datem 'Day') AS weekday
+    FROM daily_data 
+),
+add_more_features AS (
+    SELECT *
+		, (CASE 
+			WHEN month_name in ('December', 'January', 'February') THEN 'winter'
+			WHEN month_name in ('March', 'April', 'May') THEN 'spring'
+            WHEN month_name in ('June', 'July', 'August') THEN 'summer'
+            WHEN month_name in ('September', 'October', 'November') THEN 'autumn'
+		END) AS season
+    FROM add_features
+)
+SELECT *
+FROM add_more_features
+ORDER BY date
